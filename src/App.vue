2a1b85f20e94d07671bccf690f15c28bc4342366
lismgmk <lismgmk2@gmail.com>
2021-11-18 18:45:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="post" v-for="post in posts">
+    <div><strong>Name: </strong>{{post.name}}</div>
+    <div><strong>Description: </strong> {{post.body}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      posts: [
+        {id: 1, name: 'JS is a single flow dynamic typical language', body: "Description 1"},
+        {id: 2, name: 'Title 2', body: "Description 2"},
+        {id: 3, name: 'Title 3', body: "Description 3"},
+      ]
+    }
+  },
+  methods: {
+    addLike (){
+      this.likes += 1
+    },
+    addDislike (){
+     this.dislikes += 1
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.post {
+  margin-top: 15px;
+  padding: 15px;
+  border: 1px solid teal;
+}
+</style>
+
