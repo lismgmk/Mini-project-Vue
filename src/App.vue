74ba673a4a14c8708ff01e452f316d3b073ae83c
lismgmk <lismgmk2@gmail.com>
2021-11-23 18:54:53 +0300

    add pagination

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@
           @update:model-value="setSelected"
       />
     </div>
+    <my-input
+        v-model="searchQuery"
+    />
+
     <my-dialog
         v-bind:show="showDialog"
         v-if="showDialog"
@@ -18,14 +22,25 @@
       />
     </my-dialog>
     <div v-if="!loading">
+
       <post-list
           v-if="posts.length > 0"
-          v-bind:posts="sortedPosts"
+          v-bind:posts="sortSearch"
           @remove="removePost"
       />
       <h4 class="app__empty-list" v-else>This list is empty</h4>
     </div>
     <h3 v-else><strong>LOADING...</strong></h3>
+    <div class="pagination__wrapper">
+      <div
+          class="pagination__item"
+          :key="pageNum"
+          @click="changePage"
+          v-for="pageNum in totalPage"
+          :class="{current_page: pageNum === page}"
+      >{{ pageNum }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,9 +50,11 @@ import PostList from "@/components/PostList"
 import MyButton from "@/components/UI/MyButton"
 import axios from "axios";
 import MySelect from "@/components/UI/MySelect";
+import MyInput from "./components/UI/MyInput";
 
 export default {
   components: {
+    MyInput,
     MySelect,
     MyButton,
     PostForm, PostList
@@ -50,7 +67,11 @@ export default {
       optionValue: '',
       optionsData: [
         'title', 'body'
-      ]
+      ],
+      searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPage: 0
     }
   },
   methods: {
@@ -67,30 +88,44 @@ export default {
     closeDialog() {
       this.showDialog = false
     },
+    changePage (){
+
+    },
     async fetchPosts() {
       try {
         this.loading = true
         setTimeout(async () => {
-          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10', {
+            params: {
+              _limit: this._limit,
+              _page: this.page
+            }
+          })
           this.posts = response.data
           this.loading = false
+          this.totalPage = Math.ceil(response.headers['x-total-count']/this.limit)
         }, 2000)
 
       } catch (e) {
         alert('Error')
       }
     },
-    setSelected(){
+    setSelected() {
       // console.log(this.optionValue)
     }
   },
   mounted() {
     this.fetchPosts()
   },
-  computed : {
+  computed: {
     sortedPosts() {
-     return [...this.posts].sort((a,b) => {
+      return [...this.posts].sort((a, b) => {
         return a[this.optionValue]?.localeCompare(b[this.optionValue])
+      })
+    },
+    sortSearch() {
+      return this.sortedPosts.filter((option) => {
+        return option.title.toLowerCase().includes(this.searchQuery.toLocaleLowerCase())
       })
     }
   },
@@ -112,5 +147,17 @@ export default {
   color: red;
 }
 
+.pagination__wrapper {
+  display: flex;
+margin-top: 15px;
+}
+.pagination__item {
+  border: 1px solid black;
+padding: 10px;
+}
+
+.current_page {
+  color: teal;
+}
 </style>
 
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <h3>Users list</h3>
+    <transition-group name="post-list">
       <post-item
           v-for="post in posts"
           v-bind:post="post"
           :key="post.id"
           @remove="$emit('remove', post)"
       />
+      </transition-group>
   </div>
 </template>
 
@@ -25,7 +27,20 @@ export default {
 </script>
 
 <style>
-
-
-
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.8s ease;
+}
 </style>
